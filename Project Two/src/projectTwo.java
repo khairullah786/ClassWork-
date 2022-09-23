13b0abe0b34952feee426adc4bec91c1e5ec012d
khairullah786 <112987288+khairullah786@users.noreply.github.com>
2022-09-22 20:28:40 -0400

    Add files via upload

---
+++ Project Two/src/projectTwo.java
@@ -0,0 +1,44 @@
+
+public class projectTwo {
+	
+
+	public static void main(String[] args) {
+		System.out.println("My _____ Sister accidentaly _____ed all over her new ___!");
+		
+		String adjective = "sweet";
+		String verb = "fart";
+		String noun = "car";
+		
+		System.out.println("My " + adjective + " Sister accidentaly " + verb + "ed all over her new " + noun );
+		
+		
+		
+		int a = 100 + 100;
+		
+		System.out.println("100 + 100 = " + a);
+		
+		int b;
+		
+		b = a - 25;
+		
+		System.out.println(a + " - 25 = " + b);
+		
+		int c;
+		
+		c = b * 5; 
+		
+		System.out.println(b + " * 5 = " + c);
+		
+		 c= b / 5;  
+		 
+		    System.out.println(b + " / 5 = " + c);
+		    
+		    c = a%2;  
+		    
+		    System.out.println(b + " % 2 = " + 3.5);
+	}
+}
+
+		    
+
+
